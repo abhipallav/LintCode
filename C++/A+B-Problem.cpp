05ea744102e4f8525825232f544b7eeb88713c98
Ajit Abhipallav <abhipallav.iiita@gmail.com>
2017-01-14 10:38:29 +0530

    Adding A+B Solution

---
+++ C++/A+B-Problem.cpp
@@ -0,0 +1,19 @@
+Problem: http://www.lintcode.com/en/problem/a-b-problem/
+
+class Solution {
+public:
+    /*
+     * @param a: The first integer
+     * @param b: The second integer
+     * @return: The sum of a and b
+     */
+    int aplusb(int a, int b) {
+        // write your code here, try to do it without arithmetic operators.
+        while (b != 0) {
+            int carry = a & b;
+            a ^= b;
+            b = carry << 1;
+        }
+        return a;
+    }
+};
